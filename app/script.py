b742ff589d0416261e45391891cc0db3d930e635
gunttr <maksimkosachev22@gmail.com>
2025-10-16 21:50:23 +0300

    sadasdasd

--- app/script.py
+++ app/script.py
@@ -5,4 +5,5 @@ while True:
     with open("aboba.txt", "w", encoding="utf-8") as file:
         file.write("aboba\n")
         file.write("bebris\n")
+        file.write("safsafas\n")
         sleep(5)
